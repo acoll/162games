eaf1a5d2cc2141485bbb4f875b73b863e6124481
Adam <adamcoll.ac@gmail.com>
2016-03-19 14:04:23 -0400

    highcharts and logos

--- frontend/js/views/AppLayoutView.js
+++ frontend/js/views/AppLayoutView.js
@@ -1,5 +1,6 @@
 var Mn = require('backbone.marionette');
 var Chart = require('chart.js');
+var Highcharts = require('highcharts');
 var _ = require('underscore');
 var color = require('onecolor');
 
@@ -21,6 +22,34 @@ function buildChartOpts (fn) {
 		    };
 	    })
 	};
+}
+
+ series: [{
+	            name: 'Tokyo',
+	            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+	        }, {
+	            name: 'New York',
+	            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+	        }, {
+	            name: 'Berlin',
+	            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+	        }, {
+	            name: 'London',
+	            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+	        }]
+
+function buildDataSeries (fn) {
+	return Object.keys(teams).map(key => {
+		var rgb = color(teams[key].colors[0]);
+		var highlightColor = rgb.cssa();
+		var strokeColor = rgb.cssa();
+		return {
+			name: gameData[key] ? teams[key].first_name + ' ' + teams[key].last_name : 'UNKNOWN: ' + key,
+			data: gameData[key].games.map(fn),
+			strokeColor: strokeColor,
+			pointColor: highlightColor
+	    };
+    });
 }
 
 var winPerc = buildChartOpts(g => g.wins);
@@ -46,12 +75,48 @@ module.exports = Mn.LayoutView.extend({
 		};
 	},
 	onRender: function () {
-		if(this.$el.find('#chart')[0]) {
-			var ctx = this.$el.find('#chart')[0].getContext('2d');
-			this.chart = new Chart(ctx).Line(winPerc, opts);
-		}
 
-		this.hittingChart = new Chart(this.$el.find('#hitting-chart')[0].getContext('2d')).Line(homeRuns, opts);
+		Highcharts.chart(this.$el.find('#chart')[0], {
+	        title: {
+	            text: 'Monthly Average Temperature',
+	            x: -20 //center
+	        },
+	        subtitle: {
+	            text: 'Source: WorldClimate.com',
+	            x: -20
+	        },
+	        xAxis: {
+	            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+	                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+	        },
+	        yAxis: {
+	            title: {
+	                text: 'Temperature (°C)'
+	            },
+	            plotLines: [{
+	                value: 0,
+	                width: 1,
+	                color: '#808080'
+	            }]
+	        },
+	        tooltip: {
+	            valueSuffix: '°C'
+	        },
+	        legend: {
+	            layout: 'vertical',
+	            align: 'right',
+	            verticalAlign: 'middle',
+	            borderWidth: 0
+	        },
+	        series: buildDataSeries(g => g.wins)
+	    });
+
+		// if(this.$el.find('#chart')[0]) {
+		// 	var ctx = this.$el.find('#chart')[0].getContext('2d');
+		// 	this.chart = new Chart(ctx).Line(winPerc, opts);
+		// }
+
+		// this.hittingChart = new Chart(this.$el.find('#hitting-chart')[0].getContext('2d')).Line(homeRuns, opts);
 
 	},
 	clickedTeam: function (e) {
